bf55931a30fa8aa03e99812ec6f95a7ddfd5a833
Aftab <aftabsoftware888@gmail.com>
2026-02-24 12:51:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Work from './components/Work'
+import Services from './components/Services'
+import About from './components/About'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import LenisScroll from './components/LenisScroll'
+import './components/Home.css';
+
+export default function App() {
+    return (
+        <>
+            <LenisScroll />
+            <Navbar />
+            <Header />
+            <About />
+            <Services />
+            <Work />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
